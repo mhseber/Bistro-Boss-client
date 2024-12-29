f316566a56588cfed2cf1dc5430e14dbf09d642f
mahamudul hasan seber <sebermh@gmail.com>
2024-12-29 21:31:18 +0600

    Use React-Helmet

--- src/Pages/Home/Shared/NavBar.jsx
+++ src/Pages/Home/Shared/NavBar.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 
 const NavBar = () => {
     const navOptions = <>
 
-        <li><a>Item 1</a></li>
-        <li><a>Item 1</a></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/menu">Our Menu</Link></li>
         <li><a>Item 1</a></li>
     </>
     return (
---
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -0,0 +1,13 @@
+import { Helmet } from 'react-helmet-async';
+const Menu = () => {
+    return (
+        <div>
+            <Helmet>
+                <title>BisTro Boss || Menu</title>
+            </Helmet>
+            mennnnnnnnnnu
+        </div>
+    );
+};
+
+export default Menu;
