6b3cc77428c2af4c62087c05616a9fca9a5a0fe1
mahamudul hasan seber <sebermh@gmail.com>
2024-12-30 00:19:46 +0600

    Use React-parallax

--- src/Pages/Home/Shared/Cover/Cover.jsx
+++ src/Pages/Home/Shared/Cover/Cover.jsx
@@ -1,9 +1,30 @@
+import { Parallax } from 'react-parallax';
+const Cover = ({ img, title }) => {
 
-const Cover = () => {
     return (
-        <div>
+        <Parallax
+            blur={{ min: -50, max: 50 }}
+            bgImage={img}
+            bgImageAlt="the menu"
+            strength={-200}
+        >
+            <div>
+                <div>
+                    <div className="hero-overlay bg-opacity-60"></div>
+                    <div className="hero-content text-neutral-content text-center">
+                        <div className="max-w-md">
+                            <h1 className="mb-5 text-5xl font-bold uppercase">{title}</h1>
+                            <p className="mb-5">
+                                Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                                quasi. In deleniti eaque aut repudiandae et a id nisi.
+                            </p>
 
-        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Parallax>
+
     );
 };
 
--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,11 +1,19 @@
 import { Helmet } from 'react-helmet-async';
+import Cover from '../../Home/Shared/Cover/Cover';
+import menuImg from '../../../assets/menu/banner3.jpg'
+import PopularMenu from '../../Home/PopularMenu/PopularMenu';
 const Menu = () => {
     return (
         <div>
             <Helmet>
                 <title>BisTro Boss || Menu</title>
             </Helmet>
-            mennnnnnnnnnu
+            <Cover img={menuImg} title="our menu"></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover img={menuImg} title="our menu"></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover img={menuImg} title="our menu"></Cover>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
