0389c015dff94612ce33030f10cec9293a3c16c8
mahamudul hasan seber <sebermh@gmail.com>
2024-12-31 22:01:14 +0600

    npm install react-simple-captcha

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDGoR1uesxhRo9jJtmv2MK46Wd4BUe872I",
+    authDomain: "bistro-boss-3ebea.firebaseapp.com",
+    projectId: "bistro-boss-3ebea",
+    storageBucket: "bistro-boss-3ebea.firebasestorage.app",
+    messagingSenderId: "514548455348",
+    appId: "1:514548455348:web:88f855def21fa10c7104e1"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
