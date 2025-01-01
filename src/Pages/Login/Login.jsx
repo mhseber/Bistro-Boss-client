faf3aafe255ef27b41afdd535c8211cb4c8feeb5
mahamudul hasan seber <sebermh@gmail.com>
2025-01-01 15:40:02 +0600

    Create PrivateRouter

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect, useState } from 'react';
 import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
 import { AuthContext } from '../../Provider/AuthProvider';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
 import Swal from 'sweetalert2';
 
@@ -9,6 +9,12 @@ const Login = () => {
 
     const [disabled, setDisabled] = useState(true);
     const { signIn } = useContext(AuthContext);
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const from = location.state?.from?.pathname || "/";
+
+
     useEffect(() => {
         loadCaptchaEnginge(6);
     }, []);
@@ -40,6 +46,7 @@ const Login = () => {
                       `
                     }
                 });
+                navigate(from, { replace: true });
             });
     };
 
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -7,6 +7,8 @@ import Menu from "../Pages/Menu/Menu/Menu";
 import Order from "../Pages/Order/Order/Order";
 import Login from "../Pages/Login/Login";
 import SignUp from "../Pages/SignUp/SignUp";
+import PrivateRouter from "./PrivateRouter";
+import Secret from "../Pages/Home/Shared/Secret/Secret";
 
 export const router = createBrowserRouter([
     {
@@ -32,6 +34,10 @@ export const router = createBrowserRouter([
             {
                 path: 'signup',
                 element: <SignUp></SignUp>
+            },
+            {
+                path: 'secret',
+                element: <PrivateRouter><Secret></Secret></PrivateRouter>
             }
         ]
     },
