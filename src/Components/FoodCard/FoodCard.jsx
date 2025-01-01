bd90154a982b5ed14dbbee40326d8014f4224c3f
mahamudul hasan seber <sebermh@gmail.com>
2025-01-02 02:09:59 +0600

    Use Axios

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -1,6 +1,7 @@
 import Swal from "sweetalert2";
 import useAuth from "../../Hooks/useAuth";
 import { useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 
 const FoodCard = ({ item }) => {
@@ -20,6 +21,19 @@ const FoodCard = ({ item }) => {
                 image,
                 price
             }
+            axios.post('http://localhost:5000/carts', cartItem)
+                .then(res => {
+                    console.log(res.data)
+                    if (res.data.insertedId) {
+                        Swal.fire({
+                            position: "top-end",
+                            icon: "success",
+                            title: `${name} added to your cart`,
+                            showConfirmButton: false,
+                            timer: 1500
+                        });
+                    }
+                })
         }
         else {
             Swal.fire({
