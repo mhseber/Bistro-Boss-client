779f67f08f00e32d05b78dd207d9075e9b884f01
mahamudul hasan seber <sebermh@gmail.com>
2025-01-09 13:47:22 +0600

    CheckoutForm Page Done

--- src/Pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/Pages/Dashboard/Payment/CheckoutForm.jsx
@@ -4,6 +4,7 @@ import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 import useCart from "../../../Hooks/useCart";
 import useAuth from "../../../Hooks/useAuth";
 import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
 
 const CheckoutForm = () => {
     const [error, setError] = useState('');
@@ -14,6 +15,7 @@ const CheckoutForm = () => {
     const axiosSecure = useAxiosSecure();
     const { user } = useAuth();
     const [cart, refetch] = useCart();
+    const navigate = useNavigate();
     const totalPrice = cart.reduce((total, item) => total + item.price, 0)
     useEffect(() => {
         if (totalPrice > 0) {
@@ -93,6 +95,7 @@ const CheckoutForm = () => {
                         showConfirmButton: false,
                         timer: 1500
                     });
+                    navigate('/dashboard/paymentHistory')
                 }
             }
         }
